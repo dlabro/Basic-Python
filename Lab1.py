a3be21a16fb201718766c6a0abe5c93ab4e6b08f
dlabro <David@David-Labros-MacBook-Air.local>
2016-02-03 10:06:04 +0000

    1st Program
    Printing Calculation

---
+++ Lab1.py
@@ -0,0 +1,8 @@
+v = 25 # Speed in m/s
+u = 0 # Initial velocity in m/s
+t = 10 # Time in seconds
+a = 0.0
+
+a=(v-u)/t
+print('The acceleration is %i' % a)
+print('The acceleration is also {}'.format(a))
